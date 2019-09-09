1124c0191365102ed30f830a8ffd9ec436507168
larrygf02 <raulgf_02@hotmail.com>
2019-09-08 22:31:31 -0500

    Crud persons

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,50 @@
 const express = require('express')
 const app = express()
+const con  = require('../db/connection.js')
 
 app.post('/persons', (req,res) => {
     let body  = req.body;
-    console.log(body);
-    res.json({
-        ok: true,
-        data: '2'
+    if (body.name === undefined || body.surname === undefined || body.dni === undefined) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Faltan completar datos'
+            }
+        })
+    }else{
+        con.query('INSERT INTO persons (name, surname, dni) VALUES (?,?,?)', [body.name, body.surname, body.dni], (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err: {
+                        message: 'Error al agregar la persona'
+                    }
+                })
+            }else{
+                res.json({
+                    ok: true,
+                    message: 'Agregado Correctamente'
+                })
+            }
+        })
+    }
+})
+
+app.get('/persons', (req, res) => {
+    con.query('SELECT * FROM persons', (err, results) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err: {
+                    message: 'Error en la consulta'
+                }
+            })
+        }else{
+            res.json({
+                ok: true,
+                data: results
+            })
+        }
     })
 })
 
--- src/server.js
+++ src/server.js
@@ -1,28 +1,17 @@
 const express = require('express')
 const app = express()
-var mysql = require('mysql');
-
-//para las rutas
-app.use(require('./routes/index'))
 
 
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-
+//para las rutas
+app.use(require('./routes/index'))
 
 app.get('/', function(req, res) {
     res.json('Hola mundo')
 })
-
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "node-mysql"
-});
 
 app.listen(3000, () => {
     console.log('Escuchando el puerto 3000');
